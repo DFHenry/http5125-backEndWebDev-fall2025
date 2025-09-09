d9ff497f6c01cc9da57bf6bde5d16d8ae7d5e3b3
David Henry <david_freeman_henry@hotmail.com>
2025-09-09 11:19:40 -0400

    added previous files/folders to repository

---
+++ Week1/Week1Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿//get user health
+Console.WriteLine("Input your health");
+
+string userInput;
+string health;
+
+userInput = Console.ReadLine();
+
+Console.WriteLine("user's health is");
+Console.WriteLine(userInput);
+health = userInput;
+
+
+//get user name
+Console.WriteLine("what is your name?");
+string userInput2;
+string name;
+
+userInput2 = Console.ReadLine();
+
+Console.WriteLine("Hello, ");
+Console.WriteLine(userInput2);
+Console.WriteLine("!");
+name = userInput;
+
