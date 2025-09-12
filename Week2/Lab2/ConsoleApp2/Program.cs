7af2b4091016c81c2d256cdb04c3b7896301a087
David Henry <david_freeman_henry@hotmail.com>
2025-09-12 16:50:43 -0400

    Added Week 2 work

---
+++ Week2/Lab2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*float monsterHealth = 50f;
+float monsterDef = 0.8f;
+float playerDamage;
+
+Console.Write("Input your damage: ");
+
+playerDamage = Convert.ToInt32(Console.ReadLine()) * monsterDef;
+
+monsterHealth -= playerDamage;
+
+Console.WriteLine("Hit! You did " + playerDamage + " damage! Monster Health is now " + monsterHealth + ".");*/
+
+int curHealth = 30; 
+int maxHealth = 100;
+
+int healPotVal = 20;
+int potsUsed;
+int healing;
+
+Console.Write("How many potions do you want to use? ");
+potsUsed = Convert.ToInt32(Console.ReadLine());
+healing = healPotVal * potsUsed;
+curHealth += healing;
+
+if (curHealth + healing > maxHealth)
+{
+    curHealth = maxHealth;
+}
+
+if (curHealth + healing <= maxHealth)
+{
+    curHealth += healing;
+}
+
+Console.WriteLine("You drank " + potsUsed + " potions, healing you for " + healing + "!");
+Console.WriteLine("Your health is now " + curHealth + "!");
