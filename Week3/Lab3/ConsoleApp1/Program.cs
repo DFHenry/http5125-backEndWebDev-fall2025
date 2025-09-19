0d022a67bea61ed713a32eae86e1606fdab31c5a
David Henry <david_freeman_henry@hotmail.com>
2025-09-19 13:52:34 -0400

    added week 3 files

---
+++ Week3/Lab3/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿int minYear = 2008;
+int curYear = 2025;
+int userYear;
+
+string password = "password";
+string passEntry;
+
+Console.WriteLine("Please enter the year you were born.");
+
+userYear = Convert.ToInt32(Console.ReadLine());
+
+if (userYear <= minYear)
+{
+    Console.WriteLine("Age confirmed! Please enter password");
+    passEntry = Console.ReadLine();
+
+    if (passEntry == password)
+    {
+        Console.WriteLine("Password verified. Welcome into the server!");
+    }
+    else if (passEntry != password)
+    {
+        Console.WriteLine("Password incorrect. Entry denied.");
+    }
+}
+
+else if (userYear > curYear)
+{
+    Console.WriteLine("Invalid entry!");
+}
+
+else if (userYear > minYear)
+{
+    Console.WriteLine("Sorry, you are not verified as an adult.");
+}
